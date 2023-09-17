ae348b30bd34926479d784a86850c84291df2c09
gyuzero <gyuzero0103@gmail.com>
2023-09-17 16:59:05 +0900

    initial commit

---
+++ src/main/java/com/gyuzero/springbootvideojs/SpringBootVideoJsApplication.java
@@ -0,0 +1,13 @@
+package com.gyuzero.springbootvideojs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVideoJsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootVideoJsApplication.class, args);
+    }
+
+}
