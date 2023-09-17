07d0f9fcd6359edbc3939e7532c5340f4f40ee06
gyuzero <gyuzero0103@gmail.com>
2023-09-17 23:25:25 +0900

    Resource 경로 yml 파일에서 읽어오도록 수정

--- src/main/java/com/gyuzero/springbootvideojs/config/WebConfig.java
+++ src/main/java/com/gyuzero/springbootvideojs/config/WebConfig.java
@@ -1,16 +1,26 @@
 package com.gyuzero.springbootvideojs.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.File;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    private final Environment env;
+
+    public WebConfig(Environment env) {
+        this.env = env;
+    }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String fileStorePath = env.getProperty("file.store.path");
         registry
-                .addResourceHandler("/Users/gyuyoung/upload/**")
-                .addResourceLocations("file:///Users/gyuyoung/upload/");
+                .addResourceHandler(fileStorePath + "/**")
+                .addResourceLocations("file://" + fileStorePath + File.separator);
     }
 }
